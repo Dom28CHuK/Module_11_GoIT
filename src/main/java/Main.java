3c023a7d338f2443309a426ec6fc65bf35c30ecd
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-05-31 16:15:58 +0300

    up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         NameIndexesOdd nameIndexesOdd = new NameIndexesOdd();
-        List<String> names = new ArrayList<>();
-        names.add("Ivan");
-        names.add("Mary");
-        names.add("David");
-        names.add("Alice");
-        names.add("Peter");
-        nameIndexesOdd.namesWithOddSubscripts(names);
+        List<String> listName = List.of("Oleg", "Ivan", "Dom", "Peter", "Olga", "Stepan");
+        nameIndexesOdd.namesWithOddSubscripts(listName);
+
+
+
     }
 }
--- src/main/java/taskOne/NameIndexesOdd.java
+++ src/main/java/taskOne/NameIndexesOdd.java
@@ -1,9 +1,14 @@
 package taskOne;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class NameIndexesOdd {
-    public void namesWithOddSubscripts(List<String> listName) {
-        System.out.println(listName);
+    public String namesWithOddSubscripts(List<String> listName) {
+        return IntStream.range(0, listName.size())
+                .filter(i -> i % 2 != 0)
+                .mapToObj(i -> (i + 1) + ". " + listName.get(i))
+                .collect(Collectors.joining(", "));
     }
 }
