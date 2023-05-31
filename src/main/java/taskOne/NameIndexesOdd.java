71eeef3d71b8d9a129c5f8a639e24f04c804aac9
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-05-31 19:38:51 +0300

    Update NameIndexesOdd.java

--- src/main/java/taskOne/NameIndexesOdd.java
+++ src/main/java/taskOne/NameIndexesOdd.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class NameIndexesOdd {
-    public String namesWithOddSubscripts(List<String> listName) {
+    public static String namesWithOddSubscripts(List<String> listName) {
         return IntStream.range(0, listName.size())
                 .filter(i -> i % 2 != 0)
                 .mapToObj(i -> (i + 1) + ". " + listName.get(i))
