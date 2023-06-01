f9a37c984fc50c327eb5ad84b3c4ac9883b7cbf5
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-06-01 08:59:26 +0300

    add TaskOne

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         NameIndexesOdd nameIndexesOdd = new NameIndexesOdd();
-        List<String> listName = List.of("Oleg", "Ivan", "Dom", "Peter", "Olga", "Stepan");
-        nameIndexesOdd.namesWithOddSubscripts(listName);
+        List<String> listName = List.of("Oleg", "Ivan", "Dom", "Peter", "Olga", "Stepan", "Vasil", "Baron", "Orel", "Burk");
+        System.out.println(nameIndexesOdd.namesWithOddSubscripts(listName));
+
     }
 }
--- src/main/java/taskOne/NameIndexesOdd.java
+++ src/main/java/taskOne/NameIndexesOdd.java
@@ -5,10 +5,10 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class NameIndexesOdd {
-    public static String namesWithOddSubscripts(List<String> listName) {
+    public String namesWithOddSubscripts(List<String> listName) {
         return IntStream.range(0, listName.size())
                 .filter(i -> i % 2 != 0)
-                .mapToObj(i -> (i + 1) + ". " + listName.get(i))
+                .mapToObj(i -> (i) + ". " + listName.get(i))
                 .collect(Collectors.joining(", "));
     }
 }
